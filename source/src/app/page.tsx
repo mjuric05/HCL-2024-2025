3d9b558c95207bfc834cd11ff9c416c9c824fe1b
fbrstilo <fbrstilo@gmail.com>
2024-10-13 12:31:15 +0200

    Added initial source files created on lab no.1

---
+++ source/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style = {{textAlign: "center"}}>
+      <h1 style = {{padding: "4rem 0", fontSize: "8rem", fontWeight: "1000"}}>Evala</h1>
+    </div>
+  );
+}
