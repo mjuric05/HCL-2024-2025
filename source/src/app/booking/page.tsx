1dcc8f664b9acf3f0189aa057186294975540a43
Mijo Jurić-Pešić <mjuric05@fesb.hr>
2024-12-17 19:03:55 +0100

    Hopefully last fix of button managing

--- source/src/app/booking/page.tsx
+++ source/src/app/booking/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import ScrollToTopOnLoad from "@/components/ScrollToTopOnLoad";
+import { useEffect } from 'react';
 
 export default function BookingPage() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col items-center p-10">
-            <ScrollToTopOnLoad />
             <h1 className="text-6xl font-extrabold tracking-tight">Booking</h1>
         </main>
     );
--- source/src/app/car_categories/page.tsx
+++ source/src/app/car_categories/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import ScrollToTopOnLoad from "@/components/ScrollToTopOnLoad";
+import { useEffect } from 'react';
 
 export default function CarCategoriesPage() {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col items-center p-10">
-            <ScrollToTopOnLoad />
             <h1 className="text-6xl font-extrabold tracking-tight">Car Categories</h1>
         </main>
     );
