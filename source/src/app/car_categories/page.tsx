66234368e64ff4f1401121dbd4d85e90a90ba875
Mijo Jurić-Pešić <mjuric05@fesb.hr>
2024-12-21 14:59:50 +0100

    Fix: thumbnail null and undefined errors

--- source/src/app/car_categories/page.tsx
+++ source/src/app/car_categories/page.tsx
@@ -1,5 +1,6 @@
 import { getStaticProps } from "@/dbConnector";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { Document } from "@contentful/rich-text-types";
 
 export default async function CarCategoriesPage() {
     const result = await getStaticProps();
@@ -13,15 +14,15 @@ export default async function CarCategoriesPage() {
                         <div key={String(car.fields.title)} className="p-4 border-2 border-[#9747FF] rounded-md hover:transform hover:translate-y-[-5px] hover:shadow-lg transition duration-300 mb-4">
                             <h3 className="text-2xl font-semibold border-b-2 border-[#9747FF] pb-2 text-center text-white">{String(car.fields.title)}</h3>
                             <div className="flex flex-col md:flex-row items-center mt-4">
-                                {car.fields.thumbnail && typeof car.fields.thumbnail === 'object' && 'fields' in car.fields.thumbnail && (
+                                {car.fields.thumbnail && typeof car.fields.thumbnail === 'object' && !Array.isArray(car.fields.thumbnail) && 'fields' in car.fields.thumbnail && car.fields.thumbnail.fields && typeof car.fields.thumbnail.fields === 'object' && 'file' in car.fields.thumbnail.fields && (
                                     <img
-                                        src={car.fields.thumbnail?.fields?.file?.url}
+                                        src={(car.fields.thumbnail.fields.file as any)?.url}
                                         alt={typeof car.fields.title === "string" ? car.fields.title : undefined}
                                         className="w-72 h-72 object-cover rounded-lg mt-4 md:mt-0 md:mr-4"
                                     />
                                 )}
                                 <div className="text-white p-4 rounded-md shadow-md flex-1 bg-transparent mt-4 md:mt-0">
-                                    {documentToReactComponents(car.fields.description)}
+                                    {car.fields.description && documentToReactComponents(car.fields.description as Document)}
                                     <p className="text-xl font-medium mt-2">Price: {typeof car.fields.price === "number" ? car.fields.price : ""}€ per Day</p>
                                 </div>
                             </div>
