86a9742d6406c08aff01d188bbdfef728300d4f9
Mijo Jurić-Pešić <mjuric05@fesb.hr>
2025-01-21 18:00:56 +0100

    SpeedInsight #2

--- source/src/app/layout.tsx
+++ source/src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Navigation } from "@/components/navigation";
 import { Footer } from "@/components/footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><Navigation />
+      <SpeedInsights />
         {children}
         <Footer />
       </body>
--- source/src/app/page.tsx
+++ source/src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Image from 'next/image';
 import Car1Image from '../images/Car 1.jpg';
 import Car2Image from '../images/Car 2.jpg';
