71526f93b95d7faf3952cc6f613f76b5e312690a
Mijo Jurić-Pešić <mjuric05@fesb.hr>
2025-01-21 17:40:49 +0100

    Fix #3, dataFetck in route.ts

--- source/src/app/api/cars/route.ts
+++ source/src/app/api/cars/route.ts
@@ -1,34 +1,10 @@
 import { getStaticProps } from "@/dbConnector";
 import { NextResponse } from "next/server";
-import { Entry } from "contentful";
-
-interface CarFields {
-    title: string;
-    description: string;
-    price: number;
-    thumbnail?: {
-        fields: {
-            file: {
-                url: string;
-            };
-        };
-    };
-    size: string;
-}
 
 export async function GET() {
     try {
         const result = await getStaticProps();
-        const cars = result.props.cars.map((entry: Entry<CarFields>) => ({
-            fields: {
-                title: String(entry.fields.title),
-                description: entry.fields.description,
-                price: Number(entry.fields.price),
-                thumbnail: entry.fields.thumbnail,
-                size: String(entry.fields.size),
-            },
-        }));
-        return NextResponse.json(cars);
+        return NextResponse.json(result.props.cars);
     } catch (error) {
         console.error("Error fetching car data:", error);
         return NextResponse.json({ error: "Failed to fetch car data" }, { status: 500 });
