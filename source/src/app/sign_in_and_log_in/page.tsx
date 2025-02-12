71a683e267e061ab0108cb156048ab4d84186e53
fbrstilo <fbrstilo@gmail.com>
2025-02-12 16:07:01 +0100

    Simplified signup/login page

--- source/src/app/sign_in_and_log_in/page.tsx
+++ source/src/app/sign_in_and_log_in/page.tsx
@@ -15,6 +15,7 @@ export default function SignInAndLogInPage() {
         email: "",
         password: ""
     });
+    const [displayLogin, setDisplayLogin] = useState(true);
 
     const titleh2ClassName = "text-[#9747FF] text-3xl md:text-4xl font-semibold mt-4 text-center";
     const titleh3ClassName = "text-[#9747FF] text-2xl font-semibold mt-4 text-center";
@@ -80,103 +81,126 @@ export default function SignInAndLogInPage() {
 
     return (
         <main className="flex min-h-screen flex-col items-center p-4 pt-24 md:p-10 md:pt-28">
-            <h2 className={titleh2ClassName}>Join Our Crew</h2>
-            <div className="flex flex-col md:flex-row justify-between w-full md:w-3/4 mt-8 space-y-8 md:space-y-0 md:space-x-8">
-                {/* Create Account */}
-                <div className={`w-full md:w-1/2 ${frameClassName}`}>
-                    <h3 className={titleh3ClassName}>Create Account</h3>
-                    <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        className={inputClassName}
-                        onChange={handleCreateAccountChange}
-                    />
-                    <input
-                        type="text"
-                        name="surname"
-                        placeholder="Surname"
-                        className={inputClassName}
-                        onChange={handleCreateAccountChange}
-                    />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        className={inputClassName}
-                        onChange={handleCreateAccountChange}
-                    />
-                    <div className="flex items-center mt-2">
+            <div className="flex flex-col md:flex-row justify-center w-full md:w-3/4 mt-8 space-y-8 md:space-y-0 md:space-x-8 mx-auto">
+                {displayLogin ? (
+                    // Log In
+                    <div className={`w-full md:w-1/2 ${frameClassName}`}>
+                        <h3 className={titleh3ClassName}>Log In</h3>
+                        <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
                         <input
-                            type={showPasswordCreate ? "text" : "password"}
-                            name="password"
-                            placeholder="Password"
-                            className={inputClassName}
-                            onChange={handleCreateAccountChange}
-                        />
-                        <button
-                            type="button"
-                            onClick={() => setShowPasswordCreate(!showPasswordCreate)}
-                            className="ml-2 p-1 bg-white rounded-lg flex items-center justify-center"
-                        >
-                            {showPasswordCreate ? EyeOpenIcon : EyeClosedIcon}
-                        </button>
-                    </div>
-                    <div className="flex justify-center mt-4">
-                        <button
-                            type="button"
-                            className={`p-2 w-1/2 rounded-md text-white ${isCreateAccountDisabled
-                                ? "bg-gray-400 cursor-not-allowed"
-                                : "bg-green-500 hover:bg-green-600"
-                                }`}
-                            disabled={isCreateAccountDisabled}
-                        >
-                            Create Account
-                        </button>
-                    </div>
-                </div>
-
-                {/* Log In */}
-                <div className={`w-full md:w-1/2 ${frameClassName}`}>
-                    <h3 className={titleh3ClassName}>Log In</h3>
-                    <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        className={inputClassName}
-                        onChange={handleLoginChange}
-                    />
-                    <div className="flex items-center mt-2">
-                        <input
-                            type={showPasswordLogin ? "text" : "password"}
-                            name="password"
-                            placeholder="Password"
+                            type="email"
+                            name="email"
+                            placeholder="Email"
                             className={inputClassName}
                             onChange={handleLoginChange}
                         />
-                        <button
-                            type="button"
-                            onClick={() => setShowPasswordLogin(!showPasswordLogin)}
-                            className="ml-2 p-1 bg-white rounded-lg flex items-center justify-center"
-                        >
-                            {showPasswordLogin ? EyeOpenIcon : EyeClosedIcon}
-                        </button>
+                        <div className="flex items-center mt-2">
+                            <input
+                                type={showPasswordLogin ? "text" : "password"}
+                                name="password"
+                                placeholder="Password"
+                                className={inputClassName}
+                                onChange={handleLoginChange}
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPasswordLogin(!showPasswordLogin)}
+                                className="ml-2 p-1 bg-white rounded-lg flex items-center justify-center"
+                            >
+                                {showPasswordLogin ? EyeOpenIcon : EyeClosedIcon}
+                            </button>
+                        </div>
+                        <div className="flex justify-center mt-4">
+                            <button
+                                type="button"
+                                className={`p-2 w-1/2 rounded-md text-white ${isLoginDisabled
+                                    ? "bg-gray-400 cursor-not-allowed"
+                                    : "bg-green-500 hover:bg-green-600"
+                                    }`}
+                                disabled={isLoginDisabled}
+                            >
+                                Log In
+                            </button>
+                        </div>
+                        <div className="flex justify-center mt-4">
+                            <span className="text-black dark:text-white"></span>
+                                Don't have an account?{" "}
+                                <button
+                                    type="button"
+                                    className="text-[#2A00B3] dark:text-[#9747FF] ml-1"
+                                    onClick={() => setDisplayLogin(false)}
+                                >
+                                    Create account
+                                </button>
+                        </div>
                     </div>
-                    <div className="flex justify-center mt-4">
-                        <button
-                            type="button"
-                            className={`p-2 w-1/2 rounded-md text-white ${isLoginDisabled
-                                ? "bg-gray-400 cursor-not-allowed"
-                                : "bg-green-500 hover:bg-green-600"
-                                }`}
-                            disabled={isLoginDisabled}
-                        >
-                            Log In
-                        </button>
+                ) : (
+                    // Create Account
+                    <div className={`w-full md:w-1/2 ${frameClassName}`}>
+                        <h3 className={titleh3ClassName}>Create Account</h3>
+                        <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            className={inputClassName}
+                            onChange={handleCreateAccountChange}
+                        />
+                        <input
+                            type="text"
+                            name="surname"
+                            placeholder="Surname"
+                            className={inputClassName}
+                            onChange={handleCreateAccountChange}
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className={inputClassName}
+                            onChange={handleCreateAccountChange}
+                        />
+                        <div className="flex items-center mt-2">
+                            <input
+                                type={showPasswordCreate ? "text" : "password"}
+                                name="password"
+                                placeholder="Password"
+                                className={inputClassName}
+                                onChange={handleCreateAccountChange}
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPasswordCreate(!showPasswordCreate)}
+                                className="ml-2 p-1 bg-white rounded-lg flex items-center justify-center"
+                            >
+                                {showPasswordCreate ? EyeOpenIcon : EyeClosedIcon}
+                            </button>
+                        </div>
+                        <div className="flex justify-center mt-4">
+                            <button
+                                type="button"
+                                className={`p-2 w-1/2 rounded-md text-white ${isCreateAccountDisabled
+                                    ? "bg-gray-400 cursor-not-allowed"
+                                    : "bg-green-500 hover:bg-green-600"
+                                    }`}
+                                disabled={isCreateAccountDisabled}
+                            >
+                                Create Account
+                            </button>
+                        </div>
+                        <div className="flex justify-center mt-4">
+                            <span className="text-black dark:text-white"></span>
+                                Already have an account?{" "}
+                                <button
+                                    type="button"
+                                    className="text-[#2A00B3] dark:text-[#9747FF] ml-1"
+                                    onClick={() => setDisplayLogin(true)}
+                                >
+                                    Log in
+                                </button>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
 
             {/* Rules */}
@@ -184,7 +208,7 @@ export default function SignInAndLogInPage() {
                 <h3 className={titleh3ClassName}>Rules</h3>
                 <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
                 <ul className="list-disc list-inside text-black dark:text-white">
-                    <li>Email has to be in a valid format: example@gmail.com</li>
+                    <li>Email has to be in a valid format: name@domain.com</li>
                     <li>
                         Your password has to be at least 8 characters long, contain at least 1
                         uppercase letter, 1 lowercase letter, 1 number, and 1 special character
