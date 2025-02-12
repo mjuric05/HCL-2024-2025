622dfc3be23d321cf51300319e0dbfe577fd8e55
fbrstilo <fbrstilo@gmail.com>
2025-02-12 22:04:11 +0100

    Input validation and error displaying

--- source/src/app/sign_in_and_log_in/page.tsx
+++ source/src/app/sign_in_and_log_in/page.tsx
@@ -16,11 +16,19 @@ export default function SignInAndLogInPage() {
         password: ""
     });
     const [displayLogin, setDisplayLogin] = useState(true);
+    const [validateInputs, setValidateInputs] = useState({
+        email: true,
+        password: true,
+        firstName: true,
+        lastName: true
+    });
 
     const titleh2ClassName = "text-[#9747FF] text-3xl md:text-4xl font-semibold mt-4 text-center";
     const titleh3ClassName = "text-[#9747FF] text-2xl font-semibold mt-4 text-center";
-    const inputClassName = "mt-2 p-2 border-4 border-[#9747FF] rounded-md w-full text-black placeholder-black";
+    const inputClassName = "mt-2 p-2 border-4 rounded-md w-full text-black placeholder-black";
     const frameClassName = "border-2 border-[#9747FF] rounded-md p-4";
+    const borderNormal = "border-[#9747FF]";
+    const borderError = "border-red-500"; // check if color is ok
 
     const handleCreateAccountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -44,7 +52,7 @@ export default function SignInAndLogInPage() {
     };
 
     const isValidPassword = (password: string) => {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])[A-Za-z\d\S]{8,}$/;
         return passwordRegex.test(password);
     };
 
@@ -81,25 +89,34 @@ export default function SignInAndLogInPage() {
 
     return (
         <main className="flex min-h-screen flex-col items-center p-4 pt-24 md:p-10 md:pt-28">
-            <div className="flex flex-col md:flex-row justify-center w-full md:w-3/4 mt-8 space-y-8 md:space-y-0 md:space-x-8 mx-auto">
-                {displayLogin ? (
+            <div className="flex flex-col md:flex-row justify-center w-full md:w-3/4 max-w-lg mt-8 space-y-8 md:space-y-0 md:space-x-8 mx-auto">
+                {displayLogin ? ( // Display Login or Create Account
                     // Log In
-                    <div className={`w-full md:w-1/2 ${frameClassName}`}>
+                    <div className={`w-full ${frameClassName}`}>
                         <h3 className={titleh3ClassName}>Log In</h3>
                         <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
                         <input
                             type="email"
                             name="email"
                             placeholder="Email"
-                            className={inputClassName}
-                            onChange={handleLoginChange}
+                            className={`${inputClassName} ${validateInputs.email ? borderNormal : borderError}`}
+                            onChange={(e) => {
+                                handleLoginChange(e);
+                                setValidateInputs((prev) => ({
+                                    ...prev,
+                                    email: isValidEmail(e.target.value)
+                                }));
+                            }}
                         />
+                        {!validateInputs.email && (
+                            <p className="text-red-500 text-sm">Email address needs to be of a valid format: user@domain.com</p>
+                        )}
                         <div className="flex items-center mt-2">
                             <input
                                 type={showPasswordLogin ? "text" : "password"}
                                 name="password"
                                 placeholder="Password"
-                                className={inputClassName}
+                                className={`${inputClassName} ${borderNormal}`}
                                 onChange={handleLoginChange}
                             />
                             <button
@@ -124,72 +141,109 @@ export default function SignInAndLogInPage() {
                         </div>
                         <div className="flex justify-center mt-4">
                             <span className="text-black dark:text-white"></span>
-                                Don't have an account?{" "}
-                                <button
-                                    type="button"
-                                    className="text-[#2A00B3] dark:text-[#9747FF] ml-1"
-                                    onClick={() => setDisplayLogin(false)}
-                                >
-                                    Create account
-                                </button>
+                            Don't have an account?{" "}
+                            <button
+                                type="button"
+                                className="text-[#2A00B3] dark:text-[#9747FF] ml-1"
+                                onClick={() => setDisplayLogin(false)}
+                            >
+                                Create account
+                            </button>
                         </div>
                     </div>
                 ) : (
                     // Create Account
-                    <div className={`w-full md:w-1/2 ${frameClassName}`}>
-                        <h3 className={titleh3ClassName}>Create Account</h3>
-                        <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder="Name"
-                            className={inputClassName}
-                            onChange={handleCreateAccountChange}
-                        />
-                        <input
-                            type="text"
-                            name="surname"
-                            placeholder="Surname"
-                            className={inputClassName}
-                            onChange={handleCreateAccountChange}
-                        />
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                            className={inputClassName}
-                            onChange={handleCreateAccountChange}
-                        />
-                        <div className="flex items-center mt-2">
+                    <div>
+                        <div className={`w-full ${frameClassName}`}>
+                            <h3 className={titleh3ClassName}>Create Account</h3>
+                            <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
                             <input
-                                type={showPasswordCreate ? "text" : "password"}
-                                name="password"
-                                placeholder="Password"
-                                className={inputClassName}
-                                onChange={handleCreateAccountChange}
+                                type="text"
+                                name="name"
+                                placeholder="Name"
+                                className={`${inputClassName} ${validateInputs.firstName ? borderNormal : borderError}`}
+                                onChange={(e) => {
+                                    handleCreateAccountChange(e);
+                                    setValidateInputs((prev) => ({
+                                        ...prev,
+                                        firstName: e.target.value.trim() !== ""
+                                    }));
+                                }}
                             />
-                            <button
-                                type="button"
-                                onClick={() => setShowPasswordCreate(!showPasswordCreate)}
-                                className="ml-2 p-1 bg-white rounded-lg flex items-center justify-center"
-                            >
-                                {showPasswordCreate ? EyeOpenIcon : EyeClosedIcon}
-                            </button>
-                        </div>
-                        <div className="flex justify-center mt-4">
-                            <button
-                                type="button"
-                                className={`p-2 w-1/2 rounded-md text-white ${isCreateAccountDisabled
-                                    ? "bg-gray-400 cursor-not-allowed"
-                                    : "bg-green-500 hover:bg-green-600"
-                                    }`}
-                                disabled={isCreateAccountDisabled}
-                            >
-                                Create Account
-                            </button>
-                        </div>
-                        <div className="flex justify-center mt-4">
-                            <span className="text-black dark:text-white"></span>
+                            {!validateInputs.firstName && (
+                                <p className="text-red-500 text-sm">Name cannot be empty</p>
+                            )}
+                            <input
+                                type="text"
+                                name="surname"
+                                placeholder="Surname"
+                                className={`${inputClassName} ${validateInputs.lastName ? borderNormal : borderError}`}
+                                onChange={(e) => {
+                                    handleCreateAccountChange(e);
+                                    setValidateInputs((prev) => ({
+                                        ...prev,
+                                        lastName: e.target.value.trim() !== ""
+                                    }));
+                                }}
+                            />
+                            {!validateInputs.lastName && (
+                                <p className="text-red-500 text-sm">Surname cannot be empty</p>
+                            )}
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                                className={`${inputClassName} ${validateInputs.email ? borderNormal : borderError}`}
+                                onChange={(e) => {
+                                    handleCreateAccountChange(e);
+                                    setValidateInputs((prev) => ({
+                                        ...prev,
+                                        email: isValidEmail(e.target.value)
+                                    }));
+                                }}
+                            />
+                            {!validateInputs.email && (
+                                <p className="text-red-500 text-sm">Email address needs to be of a valid format: user@domain.com</p>
+                            )}
+                            <div className="flex items-center mt-2">
+                                <input
+                                    type={showPasswordCreate ? "text" : "password"}
+                                    name="password"
+                                    placeholder="Password"
+                                    className={`${inputClassName} ${validateInputs.password ? borderNormal : borderError}`}
+                                    onChange={(e) => {
+                                        handleCreateAccountChange(e);
+                                        setValidateInputs((prev) => ({
+                                            ...prev,
+                                            password: isValidPassword(e.target.value)
+                                        }));
+                                    }}
+                                />
+                                <button
+                                    type="button"
+                                    onClick={() => setShowPasswordCreate(!showPasswordCreate)}
+                                    className="ml-2 p-1 bg-white rounded-lg flex items-center justify-center"
+                                >
+                                    {showPasswordCreate ? EyeOpenIcon : EyeClosedIcon}
+                                </button>
+                            </div>
+                            {!validateInputs.password && (
+                                <p className="text-red-500 text-sm">Password must be at least 8 characters long, contain an uppercase letter, a lowercase letter, a number, and a special character.</p>
+                            )}
+                            <div className="flex justify-center mt-4">
+                                <button
+                                    type="button"
+                                    className={`p-2 w-1/2 rounded-md text-white ${isCreateAccountDisabled
+                                        ? "bg-gray-400 cursor-not-allowed"
+                                        : "bg-green-500 hover:bg-green-600"
+                                        }`}
+                                    disabled={isCreateAccountDisabled}
+                                >
+                                    Create Account
+                                </button>
+                            </div>
+                            <div className="flex justify-center mt-4">
+                                <span className="text-black dark:text-white"></span>
                                 Already have an account?{" "}
                                 <button
                                     type="button"
@@ -198,33 +252,21 @@ export default function SignInAndLogInPage() {
                                 >
                                     Log in
                                 </button>
+                            </div>
+                        </div>
+                        <div className={`w-full mt-8 ${frameClassName}`}>
+                            By creating your account, you accept our{" "}
+                            <a
+                                href="https://wary-bay-791.notion.site/Easy-Rent-ToS-162708c272668094ad94f550eff684f0?pvs=4"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-[#2A00B3] dark:text-[#9747FF]"
+                            >
+                                Terms of Service (ToS)
+                            </a>
                         </div>
                     </div>
                 )}
-            </div>
-
-            {/* Rules */}
-            <div className={`w-full md:w-3/4 mt-8 ${frameClassName}`}>
-                <h3 className={titleh3ClassName}>Rules</h3>
-                <hr className="border-[#9747FF] my-2" style={{ borderWidth: "2px" }} />
-                <ul className="list-disc list-inside text-black dark:text-white">
-                    <li>Email has to be in a valid format: name@domain.com</li>
-                    <li>
-                        Your password has to be at least 8 characters long, contain at least 1
-                        uppercase letter, 1 lowercase letter, 1 number, and 1 special character
-                    </li>
-                    <li>
-                        By creating your account, you accept our{" "}
-                        <a
-                            href="https://wary-bay-791.notion.site/Easy-Rent-ToS-162708c272668094ad94f550eff684f0?pvs=4"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-[#2A00B3] dark:text-[#9747FF]"
-                        >
-                            Terms of Service (ToS)
-                        </a>
-                    </li>
-                </ul>
             </div>
         </main>
     );
